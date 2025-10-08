e79ed4314c926175a12dc382b310a7c998a3fe3f
Daniel Estbar <daeb@natur.gl>
2025-10-08 17:53:23 +0200

    Created first script

---
+++ lol.R
@@ -0,0 +1,30 @@
+
+# HEADER --------------------------------------------------
+#
+# Author: Daniel Estévez-Barcia
+# email: daeb@natur.gl
+#
+#Wed Oct  8 17:48:45 2025
+#
+#
+# Script Name: 
+# Script Description: 
+# Project: 
+#
+# Source
+source("20-29_Coding/21_Source/21.02_Booting/wololo.R")
+
+# Specific packages
+
+library("awesome")
+
+
+
+# Specific functions
+
+# Specific vectors and definitions
+
+#
+#----------------------------------------------------------#
+#----------------------------------------------------------#
+#----------------------------------------------------------#
